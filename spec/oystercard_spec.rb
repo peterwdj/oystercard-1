32e95a7f956bf556fad3bceb0e64c43c53124311
Peter Johnstone <peter.wdj@gmail.com>
2017-11-01 14:21:31 +0000

    Added Rubocop to directory, ignoring files from Makers

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,15 +3,13 @@ require './lib/oystercard.rb'
 
 describe Oystercard do
 
-  describe "#balance" do
-      it 'Sets new card to have a default balance of 0' do
-        expect(subject.balance).to eq 0
-      end
+    it 'Sets new card to have a default balance of 0' do
+      expect(subject.balance).to eq 0
     end
 
   describe "#top_up" do
     it 'Adds money to card' do
-    expect{subject.top_up(1)}.to change{subject.balance }.by(1)
+    expect{ subject.top_up(1) }.to change{ subject.balance }.by(1)
     end
 
     it {is_expected.to respond_to{:top_up}.with(1).argument}
