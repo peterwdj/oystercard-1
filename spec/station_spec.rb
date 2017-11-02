f93b4399bfe562d23b7a17df049f83ce5232d381
Peter Johnstone <peter.wdj@gmail.com>
2017-11-02 09:25:59 +0000

    Refactored station spec file to avoid repetition in environment setup

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,14 +2,16 @@ require 'station'
 
 describe Station do
 
-  it "Station has a name" do
+  let(:station) do
     station = Station.new("Kings Cross", 1)
+  end
+
+  it "Station has a name" do
     expect(station.name).not_to be_empty
   end
 
   it "Station has a zone" do
-    station = Station.new("Kings Cross", 1)
     expect(station.zone).to eq 1
   end
-  
+
 end
