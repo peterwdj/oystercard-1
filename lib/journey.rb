725fc0b6d42b5861069c962f9272b999fd883d77
Tabish Raza <tabishraza@gmail.com>
2017-11-02 10:38:11 +0000

    creation of start and end journeys plus tests

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,13 +1,22 @@
 class Journey
-attr_reader :entry
-attr_reader :exit
+attr_reader :entry_station, :current_journey
+attr_reader :exit_station
 
   def initialize
-    @entry = nil
-    @exit = nil
+    @entry_station = nil
+    @exit_station = nil
   end
 
-  def current_journey(entry)
-    @entry = entry
+  def current_journey
+    { @entry_station => @exit_station }
+  end
+
+  def start_journey(entry_station)
+    @entry_station = entry_station
+  end
+
+  def end_journey(exit_station)
+    @exit_station = exit_station
+
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,10 +1,32 @@
 require 'journey'
 
 describe Journey do
-  describe '#current_journey' do
+  let(:entry_station) { double(:Aldgate) }
+  let(:exit_station) { double(:Finchley) }
+
+  describe '#start_journey' do
     context 'When adding a journey' do
       it 'Should return entry_station' do
-        expect(subject.current_journey("Aldgate")).to eq subject.entry
+        expect(subject.start_journey(entry_station)).to eq subject.entry_station
+      end
+    end
+  end
+
+
+  describe '#end_journey' do
+    context 'When adding a journey' do
+      it 'should return exit station' do
+        expect(subject.end_journey(exit_station)).to eq subject.exit_station
+      end
+    end
+  end
+
+  describe '#current_journey' do
+    context 'when checking whole journey' do
+      it 'should return complete single journey' do
+        subject.start_journey(entry_station)
+        subject.end_journey(exit_station)
+        expect(subject.current_journey). to eq {entry_station => exit_station}
       end
     end
   end
