62b06e2acd05810524fbb3c15c44b9f3cd47b3cb
Peter Johnstone <peter.wdj@gmail.com>
2017-11-02 16:44:08 +0000

    Adding tests for journey_spec

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,6 +1,7 @@
 require 'journey'
 
 describe Journey do
+  subject(:card)
   let(:entry_station) { double(:Aldgate) }
   let(:exit_station) { double(:Finchley) }
 
@@ -12,6 +13,14 @@ describe Journey do
       end
       it "Should return an empty journey when initialised" do
         expect(subject.journey).to eq []
+      end
+    end
+  end
+
+  describe '#penalty_fare' do
+    context 'Charging a penalty fare when touching in twice in a row' do
+      it "Should charge a £6 penalty fare" do
+        allow
       end
     end
   end
